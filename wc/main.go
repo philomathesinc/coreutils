02095c4c6cf9277deb037a0e7e8f0ea94c49f3a5
iamargus95 <kamathsuraj95@gmail.com>
2023-04-08 14:38:52 +0530

    add word count flag and function

--- wc/main.go
+++ wc/main.go
@@ -10,12 +10,15 @@ import (
 
 var (
 	lineCountFlag bool
+	wordCountFlag bool
 )
 
 func main() {
-	filename := os.Args[2]
 	flag.BoolVar(&lineCountFlag, "l", false, "Display the number of lines")
+	flag.BoolVar(&wordCountFlag, "w", false, "Display the number of words")
 	flag.Parse()
+	filename := os.Args[2]
+
 	if lineCountFlag {
 		lineCount, err := CountLines(filename)
 		if err != nil {
@@ -25,6 +28,14 @@ func main() {
 		fmt.Println(lineCount, filename)
 	}
 
+	if wordCountFlag {
+		lineCount, err := CountWords(filename)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Println(lineCount, filename)
+	}
 }
 
 func CountLines(filename string) (int, error) {
@@ -40,4 +51,19 @@ func CountLines(filename string) (int, error) {
 	}
 
 	return strings.Count(string(data), "\n"), nil
+}
+
+func CountWords(filename string) (int, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return 0, err
+	}
+
+	return strings.Count(string(data), " "), nil
 }
