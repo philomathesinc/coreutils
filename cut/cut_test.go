1ae5fdd89b21b78d9bdf833109a5bf3d1a8d34e0
Mriyam Tamuli <mbtamuli@gmail.com>
2023-04-21 22:34:56 +0530

    Add failing test case with incorrect input
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- cut/cut_test.go
+++ cut/cut_test.go
@@ -8,41 +8,65 @@ import (
 
 func TestFields(t *testing.T) {
 	t.Run("No delimiter", func(t *testing.T) {
-		fieldsTests := []struct {
-			name   string
+		type args struct {
 			input  string
 			fields string
-			want   string
+		}
+
+		fieldsTests := []struct {
+			name    string
+			args    args
+			want    string
+			wantErr bool
 		}{
 			{
 				"letters",
-				`a	b
+				args{
+					`a	b
 aa	bb	cc
 aaa	bbb	ccc	ddd`,
-				"2",
+					"2",
+				},
 				`b
 bb
 bbb`,
+				false,
 			},
 			{
 				"numbers",
-				`1	2
+				args{
+					`1	2
 11	22	33
 111	222	333	444`,
-				"2",
+					"2",
+				},
 				`2
 22
 222`,
+				false,
+			},
+			{
+				"unacceptable fields",
+				args{
+					`1	2
+ 11	22	33
+ 111	222	333	444`,
+					"a",
+				},
+				"",
+				true,
 			},
 		}
 
 		for _, tt := range fieldsTests {
 			t.Run(tt.name, func(t *testing.T) {
-				got := cut.Fields(tt.input, tt.fields)
-				want := tt.want
-
-				if got != want {
-					t.Errorf("got %q want %q", got, want)
+				got, err := cut.Fields(tt.args.input, tt.args.fields)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("Fields() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				if got != tt.want {
+					t.Errorf("Fields() = %v, want %v", got, tt.want)
 				}
 			})
 		}
