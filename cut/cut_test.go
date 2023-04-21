f8d57efb9fcdc00fe72ebc91a7829c30c2a027ba
Mriyam Tamuli <mbtamuli@gmail.com>
2023-04-21 22:04:59 +0530

    Add first test
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

---
+++ cut/cut_test.go
@@ -0,0 +1,22 @@
+package cut_test
+
+import (
+	"testing"
+)
+
+func TestFields(t *testing.T) {
+	t.Run("No delimiter", func(t *testing.T) {
+		got := cut.Fields(`a:b
+		aa:bb:cc
+		aaa:bbb:ccc:ddd
+		`, "2")
+		want := `b
+		bb
+		bbb
+		`
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+}
