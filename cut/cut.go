d72cb5fcf4f2ec4465c4b2ee00e78daf4ed40268
Mriyam Tamuli <mbtamuli@gmail.com>
2023-04-21 22:26:03 +0530

    Barely enough code to pass tests
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- cut/cut.go
+++ cut/cut.go
@@ -1,5 +1,19 @@
 package cut
 
+import (
+	"strconv"
+	"strings"
+)
+
 func Fields(input string, fields string) string {
-	return "b\nbb\nbbb"
+	var output []string
+	fieldNum, _ := strconv.Atoi(fields)
+	fieldIndex := fieldNum - 1
+	lines := strings.Split(input, "\n")
+	for _, line := range lines {
+		lFields := strings.Fields(line)
+		output = append(output, lFields[fieldIndex])
+	}
+
+	return strings.Join(output, "\n")
 }
