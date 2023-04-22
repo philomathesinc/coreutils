de5c84ec07e3772349d85dae30df1286c88cbd90
Mriyam Tamuli <mbtamuli@gmail.com>
2023-04-23 02:35:16 +0530

    Add happy path test for multiple ranges
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- cut/cut_test.go
+++ cut/cut_test.go
@@ -11,7 +11,7 @@ func TestFields(t *testing.T) {
 	t.Run("No delimiter", noDelimiters(t))
 	t.Run("Types of ranges", typesOfRanges(t))
 	t.Run("Delimiter specified", delimiterSpecified(t))
-	// t.Run("Multiple ranges", multipleRanges(t))
+	t.Run("Multiple ranges", multipleRanges(t))
 }
 
 func noDelimiters(t *testing.T) func(t *testing.T) {
@@ -215,11 +215,63 @@ func delimiterSpecified(t *testing.T) func(t *testing.T) {
 				"a	b\naa	bb	cc\naaa:bbb",
 				false,
 			},
+			{
+				"mixed delimiters in input",
+				args{
+					"a	b\naa	bb	cc\naaa:bbb:ccc:ddd",
+					"1-2",
+					":",
+				},
+				"a	b\naa	bb	cc\naaa:bbb",
+				false,
+			},
 		}
 
 		for _, tt := range fieldsTests {
 			t.Run(tt.name, func(t *testing.T) {
 				got, err := cut.Fields(tt.args.input, tt.args.fields, tt.args.delimiter)
+				if (err != nil) != tt.wantErr {
+					t.Errorf("Fields() error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+				diff := cmp.Diff(tt.want, got)
+				if diff != "" {
+					t.Errorf("Fields() mismatch (-want +got):\n%+v", diff)
+				}
+			})
+		}
+	}
+}
+
+func multipleRanges(t *testing.T) func(t *testing.T) {
+	t.Helper()
+
+	return func(t *testing.T) {
+		type args struct {
+			input  string
+			fields string
+		}
+
+		fieldsTests := []struct {
+			name    string
+			args    args
+			want    string
+			wantErr bool
+		}{
+			{
+				"happy path",
+				args{
+					"a	b\naa	bb	cc\naaa	bbb	ccc	ddd",
+					"1,3",
+				},
+				"a\naa	cc\naaa	ccc",
+				false,
+			},
+		}
+
+		for _, tt := range fieldsTests {
+			t.Run(tt.name, func(t *testing.T) {
+				got, err := cut.Fields(tt.args.input, tt.args.fields, "")
 				if (err != nil) != tt.wantErr {
 					t.Errorf("Fields() error = %v, wantErr %v", err, tt.wantErr)
 					return
