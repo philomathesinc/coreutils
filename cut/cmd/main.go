74ff4612a619b6c5e0264d4a23fe435ed2c5e0fe
Mriyam Tamuli <mbtamuli@gmail.com>
2023-04-26 05:18:13 +0530

    Add CLI
    Signed-off-by: Mriyam Tamuli <mbtamuli@gmail.com>

--- cut/cmd/main.go
+++ cut/cmd/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/philomathesinc/coreutils/cut"
+)
 
 func main() {
-	fmt.Println("call cut")
+	// Define flags
+	delimiterFlag := flag.String("d", "\t", "delimiter for fields")
+	fieldsFlag := flag.String("f", "", "fields or ranges to cut (ex. 1,3-5)")
+
+	flag.Parse()
+
+	// If no flags are passed, print usage
+	if flag.NFlag() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	files := flag.Args()
+
+	if flag.NArg() == 0 {
+		files = append(files, "-")
+	}
+
+	for _, filename := range files {
+		var (
+			file *os.File
+			err  error
+		)
+
+		if filename == "-" {
+			file = os.Stdin
+		} else {
+			file, err = os.Open(filename)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "%s: No such file or directory", filename)
+			}
+			defer file.Close()
+		}
+
+		bytes, err := io.ReadAll(file)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to read file")
+		}
+
+		fields, err := cut.Fields(string(bytes), *fieldsFlag, *delimiterFlag)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err.Error())
+		}
+
+		fmt.Println(fields)
+
+	}
 }
